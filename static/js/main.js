e219a45542aae608e657c75a1623238567b08e81
Adrien Anceau <adrien.anceau@gmail.com>
2015-10-25 10:45:12 +0100

    Added 'back to top' placeholder

--- static/js/main.js
+++ static/js/main.js
@@ -3,11 +3,14 @@ function init() {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
       shrinkOn = 200,
       header = document.querySelector("header");
+      backToTop = document.querySelector("scroll-button");
     if (distanceY > shrinkOn) {
       header.classList.add("smaller");
+      backToTop.classList.add("visible");
     } else {
       if (header.classList.contains("smaller")) {
         header.classList.remove("smaller");
+        backToTop.classList.remove("visible");
       }
     }
   });
